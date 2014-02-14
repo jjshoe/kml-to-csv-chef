34e1bb5f9d15c2ef7b919e3d552055d37f1f110f
Joel <jjshoe@gmail.com>
2014-02-13 18:36:01 -0600

    Version bump to make Berkshelf happy

--- Metadata.rb
+++ Metadata.rb
@@ -1,6 +1,6 @@
 name "kml-to-csv"
 maintainer "jjshoe"
 maintainer_email "jjshoe@gmail.com"
-version "0.0.1"
+version "0.0.2"
 description "Configure a kml to csv conversion machine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
