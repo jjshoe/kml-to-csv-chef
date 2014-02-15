24a8af49e8cba397f3e7c491825af2a5db2674ed
Joel <jjshoe@gmail.com>
2014-02-15 13:33:43 -0600

    Should be kml, not xml.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 package "git"
 package "libxml-xpath-perl"   
 
-git "/root/xml-to-csv" do
+git "/root/kml-to-csv" do
   repository "https://github.com/jjshoe/kml-to-csv.git"
   reference "master"
   action :sync
